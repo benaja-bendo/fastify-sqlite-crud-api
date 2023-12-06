aab40d41d318ee18c632eaa3ded25f74d5de75bf
benaja-bendo <benaja.bendo02@gmail.com>
2023-12-06 09:09:11 +0100

    Refactor SQLiteDB class and update database connection

--- src/db/SQLiteDB.ts
+++ src/db/SQLiteDB.ts
@@ -1,81 +1,32 @@
-import Knex from 'knex';
-import path from "path";
+import { Sequelize } from 'sequelize';
+import path from 'path';
 
 class SQLiteDB {
   private static instance: SQLiteDB;
-  private db: any | null = null;
+  private db: Sequelize | null = null;
   private readonly dbPath = path.join(__dirname, "../../database.db");
 
   private constructor() {
-    // this.init();
+    this.init();
   }
 
-  static async getInstance() {
+  private init() {
+    this.db = new Sequelize({
+      dialect: 'sqlite',
+      storage: this.dbPath,
+    });
+  }
+
+  static getInstance() {
     if (!SQLiteDB.instance) {
       SQLiteDB.instance = new SQLiteDB();
-      await SQLiteDB.instance.init();
     }
     return SQLiteDB.instance;
   }
 
-  private async init() {
-    this.db = Knex({
-      client: "sqlite3",
-      connection: {
-        filename: this.dbPath,
-      },
-      useNullAsDefault: true,
-    });
-
-    // Ajoutez des tables ici
-    await this.db.schema.createTableIfNotExists("users", (table: any) => {
-      table.increments("id").primary();
-      table.string("name").notNullable();
-    });
-  }
-  async enterFakeUsers() {
-    try {
-      // You can adjust the number of fake users as needed
-      const fakeUsers = [
-        { name: "John Doe" },
-        { name: "Jane Doe" },
-        { name: "Bob Smith" },
-      ];
-
-      await this.db("users").insert(fakeUsers);
-
-      console.log("Fake users added successfully.");
-    } catch (error) {
-      console.error("Error entering fake users:", error);
-    }
-  }
-
-  getDatabase() {
+  getDB() {
     return this.db;
   }
-  async disconnect() {
-    await this.db?.destroy();
-  }
-
-  // async connect() {
-  //   this.db = await open({
-  //     filename: "database.db",
-  //     driver: Database,
-  //   });
-  // }
-
-  // async disconnect() {
-  //   await this.db?.close();
-  // }
-
-  // async run(sql: string) {
-  //   await this.db?.run(sql);
-  // }
-
-  // async all(sql: string) {
-  //   return await this.db?.all(sql);
-  // }
 }
 
 export default SQLiteDB;
-
